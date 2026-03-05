8f0c893241062aca599153cef9488c9b30b72d6c
Developer <cergeyyarovikov82@gmail.com>
2026-03-05 21:17:55 +0200

    fix: use client.ready() instead of checking client.user

--- src/controllers/client/Session.tsx
+++ src/controllers/client/Session.tsx
@@ -281,6 +281,6 @@ export default class Session {
      * @returns Boolean
      */
     @computed get ready() {
-        return !!this.client?.user;
+        return this.client?.ready() ?? false;
     }
 }
